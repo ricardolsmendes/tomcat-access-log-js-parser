ca7296e6b39591efd687f076036e8f93dd0f00aa
Ricardo Mendes <ricardolsmendes@gmail.com>
2019-11-26 23:54:49 -0300

    Parse objects in their correct types

--- app/tomcat-access-log-parser.js
+++ app/tomcat-access-log-parser.js
@@ -13,29 +13,55 @@
  * %b - Bytes sent, excluding HTTP headers, or '-' if zero.
  */
 function parseCommonFormat(line) {
-    const commonFormatRegex = new RegExp('' +
-        /(?<remoteHost>.+)[\s]/.source +
-        /-[\s]/.source +
-        /(?<remoteUser>.+)[\s]/.source +
-        /\[(?<dateTime>.+)\][\s]/.source +
-        /"(?<request>.+)"[\s]/.source +
-        /(?<httpStatus>\d{3})[\s]/.source +
-        /(?<bytesSent>.+)$/.source
-    );
+  const commonFormatRegex = new RegExp('' +
+    /(?<remoteHost>.+)[\s]/.source +
+    /-[\s]/.source +
+    /(?<remoteUser>.+)[\s]/.source +
+    /\[(?<datetime>.+)\][\s]/.source +
+    /"(?<request>.+)"[\s]/.source +
+    /(?<httpStatus>\d{3})[\s]/.source +
+    /(?<bytesSent>.+)$/.source
+  );
 
-    const matches = commonFormatRegex.exec(line)
+  const matches = commonFormatRegex.exec(line)
 
-    var logData = {};
-    logData.remoteHost = matches.groups.remoteHost;
-    logData.remoteUser = matches.groups.remoteUser;
-    logData.dateTime = matches.groups.dateTime;
-    logData.request = matches.groups.request;
-    logData.httpStatus = matches.groups.httpStatus;
-    logData.bytesSent = matches.groups.bytesSent;
+  const remoteUser = matches.groups.remoteUser;
+  const bytesSent = matches.groups.bytesSent;
 
-    return JSON.stringify(logData);
+  var logData = {};
+  logData.remoteHost = matches.groups.remoteHost;
+  logData.remoteUser = remoteUser !== '-' ? remoteUser : null;
+  logData.datetime = parseCommonFormatDatetime(matches.groups.datetime);
+  logData.request = matches.groups.request;
+  logData.httpStatus = parseInt(matches.groups.httpStatus);
+  logData.bytesSent = bytesSent !== '-' ? parseInt(bytesSent) : null;
+
+  return JSON.stringify(logData);
+}
+
+function parseCommonFormatDatetime(datetimeString) {
+  const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+  const commonLogFmtRegex = new RegExp('' +
+    /(?<day>\d{2})\/(?<month>\w{3})\/(?<year>\d{4})/.source +
+    /:/.source +
+    /(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})[\s]/.source +
+    /(?<timezone>.{5})/.source
+  );
+
+  const matches = commonLogFmtRegex.exec(datetimeString)
+
+  return new Date(
+    matches.groups.year +
+    '-' + (monthNames.indexOf(matches.groups.month) + 1).toString() +
+    '-' + matches.groups.day +
+    'T' + matches.groups.hour +
+    ':' + matches.groups.minute +
+    ':' + matches.groups.second + '.000' +
+    matches.groups.timezone
+  );
 }
 
 module.exports = {
-    parseCommonFormat
+  parseCommonFormat
 }
--- test/tomcat-access-log-parser.spec.js
+++ test/tomcat-access-log-parser.spec.js
@@ -4,50 +4,62 @@ var logParser = require('../app/tomcat-access-log-parser');
 
 describe('tomcat-access-log-parser', function() {
 
-    describe('parseCommonFormat', function() {
+  describe('parseCommonFormat', function() {
 
-        it('parses the remoteHost attribute', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - - [23/Nov/2019] "GET" 200 482'));
-            assert.strictEqual(logData.remoteHost, '127.0.0.1');
-        });
-
-        it('parses the remoteUser attribute', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019] "GET" 200 482'));
-            assert.strictEqual(logData.remoteUser, 'user_id');
-        });
-
-        it('parses the dateTime attribute', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
-            assert.strictEqual(logData.dateTime, '23/Nov/2019:23:59:52 -0200');
-        });
-
-        it('parses the request attribute', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019] "GET index.html" 200 482'));
-            assert.strictEqual(logData.request, 'GET index.html');
-        });
-
-        it('parses the httpStatus attribute', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019] "GET" 200 482'));
-            assert.strictEqual(logData.httpStatus, '200');
-        });
-
-        it('parses the bytesSent attribute if value is greater than zero', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019] "GET" 200 482'));
-            assert.strictEqual(logData.bytesSent, '482');
-        });
-
-        it('parses the bytesSent attribute if value is zero', function() {
-            const logData = JSON.parse(logParser.parseCommonFormat(
-                '127.0.0.1 - user_id [23/Nov/2019] "GET" 200 -'));
-            assert.strictEqual(logData.bytesSent, '-');
-        });
-
+    it('returns a string', function() {
+      const logData = logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482');
+      assert(typeof logData === 'string');
     });
+
+    it('parses the remoteHost attribute', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.strictEqual(logData.remoteHost, '127.0.0.1');
+    });
+
+    it('parses the remoteUser attribute if user is not authenticated', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.strictEqual(logData.remoteUser, null);
+    });
+
+    it('parses the remoteUser attribute if user is authenticated', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - user_id [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.strictEqual(logData.remoteUser, 'user_id');
+    });
+
+    it('parses the datetime attribute as UTC', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.deepStrictEqual(new Date(logData.datetime), new Date('2019-11-24T01:59:52+0000'));
+    });
+
+    it('parses the request attribute', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET index.html" 200 482'));
+      assert.strictEqual(logData.request, 'GET index.html');
+    });
+
+    it('parses the httpStatus attribute', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.strictEqual(logData.httpStatus, 200);
+    });
+
+    it('parses the bytesSent attribute if value is zero', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 -'));
+      assert.strictEqual(logData.bytesSent, null);
+    });
+
+    it('parses the bytesSent attribute if value is greater than zero', function() {
+      const logData = JSON.parse(logParser.parseCommonFormat(
+        '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
+      assert.strictEqual(logData.bytesSent, 482);
+    });
+
+  });
 
 });
