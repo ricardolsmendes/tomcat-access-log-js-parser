60000566b5e37fe51f9549247ff4cdf8b9eed2f4
Ricardo Mendes <ricardolsmendes@gmail.com>
2020-01-05 17:41:18 -0300

    Standardize ES6 arrow functions usage

--- test/tomcat-access-log-parser.spec.js
+++ test/tomcat-access-log-parser.spec.js
@@ -4,61 +4,61 @@ const assert = require('assert');
 
 const { parseCommonFormat } = require('../src');
 
-describe('tomcat-access-log-parser', function() {
+describe('tomcat-access-log-parser', () => {
 
-  describe('parseCommonFormat', function() {
+  describe('parseCommonFormat', () => {
 
-    it('returns a string', function() {
+    it('returns a string', () => {
       const logData = parseCommonFormat(
         '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482');
       assert(typeof logData === 'string');
     });
 
-    it('parses the remoteHost attribute', function() {
+    it('parses the remoteHost attribute', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.strictEqual(logData.remoteHost, '127.0.0.1');
     });
 
-    it('parses the remoteUser attribute if user is not authenticated', function() {
+    it('parses the remoteUser attribute if user is not authenticated', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.strictEqual(logData.remoteUser, null);
     });
 
-    it('parses the remoteUser attribute if user is authenticated', function() {
+    it('parses the remoteUser attribute if user is authenticated', () => {
       const logData = JSON.parse(
         parseCommonFormat(
           '127.0.0.1 - user_id [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.strictEqual(logData.remoteUser, 'user_id');
     });
 
-    it('parses the datetime attribute according to universal time', function() {
+    it('parses the datetime attribute according to universal time', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.deepStrictEqual(logData.datetime, '2019-11-24T01:59:52.000Z');
     });
 
-    it('parses the request attribute', function() {
+    it('parses the request attribute', () => {
       const logData = JSON.parse(
         parseCommonFormat(
           '127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET index.html" 200 482'));
       assert.strictEqual(logData.request, 'GET index.html');
     });
 
-    it('parses the httpStatus attribute', function() {
+    it('parses the httpStatus attribute', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.strictEqual(logData.httpStatus, 200);
     });
 
-    it('parses the bytesSent attribute if value is zero', function() {
+    it('parses the bytesSent attribute if value is zero', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 -'));
       assert.strictEqual(logData.bytesSent, null);
     });
 
-    it('parses the bytesSent attribute if value is greater than zero', function() {
+    it('parses the bytesSent attribute if value is greater than zero', () => {
       const logData = JSON.parse(
         parseCommonFormat('127.0.0.1 - - [23/Nov/2019:23:59:52 -0200] "GET" 200 482'));
       assert.strictEqual(logData.bytesSent, 482);
